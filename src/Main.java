50ec9a76be2fd8cb98e8d53619cbef2686d28448
Felipe Ribeiro <lipe13silva@hotmail.com>
2024-10-04 16:52:54 -0300

    LinkedListChall

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Linked List Challenge");
+        System.out.println("Linked List Challenge from Course Java Masterclass");
     }
 }
